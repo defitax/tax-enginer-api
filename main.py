089128b7eb55cf2d11b2099aec71d1b059e4438e
Shankar Rao Mata <mshankarrao1970@gmail.com>
2024-01-11 22:03:44 -0500

    add : two new endpoints for the creating and uploading the file

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
 app = FastAPI()
 @app.get("/calculate")
 async def root():
     return {"message": "Calculating"}
+
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}    
