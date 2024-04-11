d695500967d2434490dd15f2979911d7756f1102
Shankar Rao Mata <mshankarrao1970@gmail.com>
2024-04-11 21:57:49 +0530

    removed the inapi calculation for parsing the excel

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import csv
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import FastAPI, File, UploadFile
 
 import pandas as pd
+from pydantic import BaseModel
+
+class Transaction(BaseModel):
+    date: str
+    currency: str
+    type: str
+    amount: str
+    from_account: str
+    to_account: str
+    value: str
 
 app = FastAPI()
 @app.get("/calculate")
@@ -24,27 +34,18 @@ async def create_upload_file(file: UploadFile):
 def import_csv_data(content,name):
     data = []
     print("hello",content)
-    with open(name, 'wb') as file:
-        csv_reader = csv.reader(file)
+    with open(name, newline='') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
         print("csv_reader",csv_reader)
-        #next(csv_reader)  # Skip the header row if present
+        next(csv_reader)  # Skip the header row if present
         for row in content:
+            print("lalua",row['Date'])
             data.append(row)
     print("Data",data)
     return data
 
 @app.post("/excel/")
-async def upload_excel_parser(file: UploadFile = File(...)):
-    data = []
-    content = file.file.read()
-    import_csv_data(content,file.filename)
-    print("test",file.filename)
-    print("read",content)
-    csv_reader = csv.reader(content)
-    print("csv_reader",csv_reader)
-    next(csv_reader)
-    for row in csv_reader:
-        data.append(row)
+async def upload_excel_parser(data: List[List[str]]):
     print("data",data)
     df = pd.DataFrame(data, columns=['Date', 'Currency', 'Type', 'Coin', 'Price', 'Fees', 'Total'])
     numeric_columns = ['Coin', 'Price', 'Fees', 'Total']
@@ -53,8 +54,7 @@ async def upload_excel_parser(file: UploadFile = File(...)):
     pivot_table = df.pivot_table(
     index=['Currency', 'Type'],
     values=['Coin', 'Total', 'Fees', 'Price'],
-    aggfunc={'Coin': 'sum', 'Total': 'sum', 'Fees': 'sum', 'Price': 'mean'}
-)
+    aggfunc={'Coin': 'sum', 'Total': 'sum', 'Fees': 'sum', 'Price': 'mean'})
     print("Pivot Table:")
     print(pivot_table)
     return df
